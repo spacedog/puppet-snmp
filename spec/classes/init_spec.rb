4ede8be7b611210a462ca8d98a7e4cc1ae98a75b
Anton Baranov <abaranov@linuxfoundation.org>
2015-09-29 12:05:27 -0400

    Add test for classes relationship
    Signed-off-by: Anton Baranov <abaranov@linuxfoundation.org>

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -13,6 +13,9 @@ describe 'snmp' do
   context 'with defaults for all parameters' do
     it { should contain_class('snmp') }
     it { should contain_anchor('snmp::begin') }
-    it { should contain_anchor('snmp::end') }
+    it { should contain_class('snmp::install').that_requires('Anchor[snmp::begin]') }
+    it { should contain_class('snmp::config').that_requires('Class[snmp::install]') }
+    it { should contain_class('snmp::service').that_subscribes_to('Class[snmp::config]') }
+    it { should contain_anchor('snmp::end').that_requires('Class[snmp::service]') }
   end
 end
